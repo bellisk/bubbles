6cc08a19148d39718d6d0a6675eb2bb4094b3f39
bellisk <daisywheel22@gmail.com>
2013-11-22 13:35:30 +0100

    Setting up project and app.

--- bubbles/bubblesapp/models.py
+++ bubbles/bubblesapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+INTERVALS = (
+    (1, "Every day"),
+    (3, "Every three days"),
+    (7, "Every week"),
+    (14, "Every two weeks"),
+    (30, "Every month"),
+    (90, "Every three months"),
+    (365, "Every year")
+)
+
+THRESHOLDS = (
+    (2, "twice"),
+    (5, "five times"),
+    (10, "ten times")
+)
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, related_name="profiles")
+    reminder_interval = models.IntegerField(choices=INTERVALS)
+    reminder_threshold = models.IntegerField(choices=THRESHOLDS)
+    def __unicode__(self):
+        return self.user.username
+
+class Contact(models.Model):
+    profile = models.ForeignKey(Profile, related_name="contacts")
+    name = models.CharField(max_length=200)
+    last_contact = models.DateField()
+    frequency = models.IntegerField(choices=INTERVALS)
+    def __unicode__(self):
+        return self.name
+
+
---
+++ bubbles/bubblesapp/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+from bubbles import bubblesapp
+
+urlpatterns = patterns('',
+
+)
